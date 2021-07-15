80b98293aa55c2302d0d9e2505f8f94cd29926e5
Hugo <hugo.lima.chaves@gmail.com>
2021-07-15 07:30:30 -0300

    adição de rotinas para alteração de estados

---
+++ RegistersAVR.cpp
@@ -0,0 +1,21 @@
+#include <RegistersAVR.h> 
+
+RegistersAVR::RegistersAVR()
+{
+
+}
+
+RegistersAVR::~RegistersAVR()
+{
+
+}
+
+static void RegistersAVR::setOn(int reg, int port)
+{
+	_SFR_IO8(reg) |= (1<<port);  //altrando PORTB	
+}
+
+static void RegistersAVR::setOff(int reg, int port)
+{
+	_SFR_IO8(reg) &= (1<<port)^0xFF;  //altrando PORTB
+}
---
+++ RegistersAVR.h
@@ -0,0 +1,19 @@
+#ifndef RegistersAVR_H
+#define RegistersAVR_H
+
+#ifndef ARDUINO_H
+	#define ARDUINO_H
+	#include "Arduino.h"
+#endif
+
+class RegistersAVR
+{
+	public:
+
+	RegistersAVR();
+	virtual ~RegistersAVR();
+	static void setOn(int reg, int port);
+	static void setOff(int reg, int port);
+};
+
+#endif 
