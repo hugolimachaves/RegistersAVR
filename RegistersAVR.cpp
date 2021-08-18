90552e4af63c226b63cda0f6fdb8a95f67bcf470
Hugo <hugo.lima.chaves@gmail.com>
2021-08-18 18:58:47 -0300

    versao corrigida

--- RegistersAVR.cpp
+++ RegistersAVR.cpp
@@ -10,17 +10,54 @@ RegistersAVR::~RegistersAVR()
 
 }
 
-static void RegistersAVR::setOn(int reg, int port)
+/**
+ * # Coloca em estado logico alto uma porta definida anteriormente como saida.
+ * - Primeiro argumento é o endereço absoluto do registrador de PORT no formato PORTX . Temos defines para cada um deles no formato _PORTX
+ * - Segundo argumento é a macro default do AVR para as portas no formato PORTXN, onde X é o grupo e N é o numero da porta dentro desse grupo.
+*/
+static void RegistersAVR::setOn(uint16_t regAbsolteAddress, uint8_t registerBit)
 {
-	_SFR_IO8(reg) |= (1<<port);  //altrando PORTB	
+	(*(volatile uint16_t *)( regAbsolteAddress )) |= (1<<registerBit);  
 }
 
-static void RegistersAVR::setOff(int reg, int port)
+
+/**
+ * # Coloca em estado logico baixo uma porta definida anteriormente como saida.
+ * - Primeiro argumento é o endereço absoluto do registrador de PORT no formato PORTX . Temos defines para cada um deles no formato _PORTX
+ * - Segundo argumento é a macro default do AVR para as portas no formato PORTXN, onde X é o grupo e N é o numero da porta dentro desse grupo.
+*/
+static void RegistersAVR::setOff(uint16_t regAbsolteAddress, uint8_t registerBit)
 {
-	_SFR_IO8(reg) &= (1<<port)^0xFF;  //altrando PORTB
+	(*(volatile uint16_t *)( regAbsolteAddress )) &= (1<<registerBit)^0xFF;  
 }
 
-static void RegistersAVR::setOutput(int reg, int port)
+/**
+ * # Define uma porta como entrada ou saida
+ * - Primeiro argumento é o endereço absoluto do registrador de direção DDR no formato DDRX . Temos defines para cada um deles no formato _DDRX
+ * - Segundo argumento é a macro default do AVR para as portas no formato DDRXN, onde X é o grupo e N é o numero da porta dentro desse grupo.
+ * - Terceiro argumento é a direçao da porta definida pelas seguinte macros locais _OUTPUT e _INPUT.
+ * @attention As macros locais são sempre seguidas de _ (underscore) no formato _MACRO
+*/
+static void RegistersAVR::setIO(uint16_t regAbsolteAddress, uint8_t registerBit, uint8_t direction)
 {
-	_SFR_IO8(reg) |= (1<<port);  //setando como output
+	if (direction == _INPUT) 
+	{
+		(*(volatile uint16_t *)( regAbsolteAddress )) &= (1 << registerBit)^0xFF;
+
+	} 
+	else if (direction == _OUTPUT) 
+	{
+		(*(volatile uint16_t *)(regAbsolteAddress)) |= (1<<registerBit);  //setando como input
+	}
+}
+
+/**
+ * # Verifica estado de uma porta digital. 
+ * - Primeiro argumento é o endereço absoluto do registrador PIN. Temos defines para cada um deles no formato _PINX
+ * - Segundo argumento é a macro default do AVR para as portas no formato PINXN.
+ * @attention Deve-se definir anteriormente a porta como entrada
+*/
+static bool RegistersAVR::getInput(uint16_t regAbsolteAddress, uint8_t registerBit) 
+{
+	return (regAbsolteAddress & 1<<registerBit)>>registerBit;
 }
--- RegistersAVR.h
+++ RegistersAVR.h
@@ -6,15 +6,78 @@
 	#include "Arduino.h"
 #endif
 
+#if defined(ATMEGA2560)
+
+	#define _PINA 0x20
+	#define _PINB 0x23
+	#define _PINC 0x26
+	#define _PIND 0x29
+	#define _PINE 0x2C
+	#define _PINF 0x2F
+	#define _PING 0x32
+	#define _PINH 0x100
+
+	#define _DDRA 0x21
+	#define _DDRB 0x24
+	#define _DDRC 0x27
+	#define _DDRD 0x2A
+	#define _DDRE 0x2D
+	#define _DDRF 0x30
+	#define _DDRG 0x33
+	#define _DDRH 0x101
+
+	#define _PORTA 0x22
+	#define _PORTB 0x25
+	#define _PORTC 0x28
+	#define _PORTD 0x2B
+	#define _PORTE 0x2E
+	#define _PORTF 0x31
+	#define _PORTG 0x34
+	#define _PORTH 0x102
+
+	#define SYSOFF_DIRECTION 			_INPUT //Lembrar que errei ao colocar um R0 para o terra. Trabalhar como INPUT
+	#define POWER_GOOD_DIRECTION 		_INPUT
+	#define CHARGING_DIRECTION 			_INPUT
+	#define CE_POWER_DIRECTION          _OUTPUT
+
+	#define SYSOFF_DDR_NUMBER 			DDH1
+	#define POWER_GOOD_DDR_NUMBER 		DDH7
+	#define CHARGING_DDR_NUMBER 		DDH2
+	#define CE_POWER_DDR_NUMBER 		DDH0
+
+	#define SYSOFF_DIRECTION_FAMILY 	_DDRH
+	#define POWER_GOOD_DIRECTION_FAMILY _DDRH
+	#define CHARGING_DIRECTION_FAMILY 	_DDRH
+	#define CE_POWER_DIRECTION_FAMILY 	_DDRH
+
+	#define SYSOFF_INPUT_NUMBER 		PINH1
+	#define POWER_GOOD_INPUT_NUMBER 	PINH7
+	#define CHARGING_INPUT_NUMBER 		PINH2
+	#define CE_POWER_OUTPUT_NUMBER 		PORTH0
+
+
+	#define SYSOFF_INPUT_FAMILY 		_PINH
+	#define POWER_GOOD_INPUT_FAMILY 	_PINH
+	#define CHARGING_INPUT_FAMILY 		_PINH
+	#define CE_POWER_INPUT_FAMILY 		_PORTH
+
+
+#endif
+
+#define _INPUT 0
+#define _OUTPUT 1
+
+
 class RegistersAVR
 {
 	public:
 
 	RegistersAVR();
 	virtual ~RegistersAVR();
-	static void setOn(int reg, int port);
-	static void setOff(int reg, int port);
-	static void setOutput(int reg, int port);
+	static void setOn(uint16_t regAbsolteAddress, uint8_t registerBit);
+	static void setOff(uint16_t regAbsolteAddress, uint8_t registerBit);
+	static void setIO(uint16_t regAbsolteAddress, uint8_t registerBit, uint8_t direction);
+	static bool getInput(uint16_t regAbsolteAddress, uint8_t registerBit);
 };
 
 #endif 
